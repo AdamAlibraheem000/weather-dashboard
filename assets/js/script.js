9faf67c96e3c70963512a2bd403f0435fdb0e96d
AdamAlibraheem <shrineknight000@gmail.com>
2021-12-06 15:21:14 -0500

    pulled data from lat & lon

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,32 +3,60 @@ searchHistory = [];
 
 // variables
 var cityName = "";
-var cityAPI =
-  "api.openweathermap.org/data/2.5/weather?q=" +
-  cityName +
-  "&appid=6a193b0402d92ad688c98786f9ef7e7f";
 
 var theBeer = document.querySelector(".beer");
 theBeer.addEventListener("click", function () {
   cityName = document.getElementById("userInput").value;
+  let lat = "";
+  let lon = "";
+
+  // Acquire city lat & longitude
 
   let getCityAPI =
-    "api.openweathermap.org/data/2.5/weather?q=dayton&appid=6a193b0402d92ad688c98786f9ef7e7f";
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&appid=6a193b0402d92ad688c98786f9ef7e7f";
 
   fetch(getCityAPI)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      lat = data.coord.lat;
+      lon = data.coord.lon;
+
+      cityLatLon =
+        "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&appid=6a193b0402d92ad688c98786f9ef7e7f";
+
+      fetch(cityLatLon)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          var uv = data.current.uvi;
+          console.log(uv);
+        });
     });
 
-  // var latLongFetch = fetchLatLong(getCityAPI);
-  // console.log(latLongFetch);
+  // cityLatLon =
+  //   "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+  //   lat +
+  //   "&lon=" +
+  //   lon +
+  //   "&appid=6a193b0402d92ad688c98786f9ef7e7f";
+
+  // fetch(cityLatLon)
+  //   .then(function (response) {
+  //     return response.json();
+  //   })
+  //   .then(function (newData) {
+  //     var uv = newData.uvi;
+  //     console.log(uv);
+  //   });
 });
 
-// function fetchLatLong(getCityAPI) {
-//   fetch(getCityAPI).then(function (response) {
-//     return response.json();
-//   });
-// }
+// https://api.openweathermap.org/data/2.5/onecall?lat=41.85&lon=-87.65&appid=6a193b0402d92ad688c98786f9ef7e7f
